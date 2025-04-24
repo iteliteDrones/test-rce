3de027d40f323f04bd0086ab5d0407eb13ef90b1
itelite <146063360+iteliteDrones@users.noreply.github.com>
2025-04-24 14:38:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req, res) => res.end("Express on Vercel"));
 
 app.get("/rce", (req, res) => {
-    res.download("./test.php")
+    res.download("./test.phar")
 });
 
 app.listen(3000)
